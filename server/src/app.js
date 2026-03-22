36824c44592922a5b51621fcd808719ad2946796
Hemant h <hemanthkalapatu2006@gmail.com>
2026-03-22 14:13:40 +0900

    running server problem solved

--- server/src/app.js
+++ server/src/app.js
@@ -4,33 +4,34 @@ import authRoutes from './routes/authRoutes.js'
 import todoRoutes from './routes/todoRoutes.js'
 import { errorHandler, notFoundHandler } from './middleware/errorMiddleware.js'
 import dotenv from "dotenv";
+
 dotenv.config();
+
 const app = express()
-import pkg from "pg";
-const { Client } = pkg;
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-
-
+// middleware
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true
 }));
+
 app.use(express.json())
 
+
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+})
+
+// existing health route
 app.get('/api/health', (req, res) => {
   res.status(200).json({ message: 'Server is running.' })
 })
 
+// routes
 app.use('/api/auth', authRoutes)
 app.use('/api/todos', todoRoutes)
 
+// error handlers
 app.use(notFoundHandler)
 app.use(errorHandler)
 
